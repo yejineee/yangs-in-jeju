9453702b55716c6b34009fed7d2b94f2171d54b3
yejineee <yygeniee@gmail.com>
2022-05-07 00:35:14 +0900

    Feat: 섬 아래 검은색 돌 추가

---
+++ src/components/Island.tsx
@@ -0,0 +1,13 @@
+import Floor from './Floor';
+import Rock from './Rock';
+
+const Island = () => {
+  return (
+    <>
+      <Floor />
+      <Rock />
+    </>
+  );
+};
+
+export default Island;
--- src/routes/JejuAlbum/index.jsx
+++ src/routes/JejuAlbum/index.jsx
@@ -1,14 +1,14 @@
 import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import Floor from '../../components/Floor';
+import Island from '../../components/Island';
 import Light from '../../systems/Light';
 
 const JejuAlbum = () => {
   return (
-    <Canvas camera={{ fov: 50, near: 0.1, far: 1000, position: [30, 30, 30] }} shadows={true}>
+    <Canvas camera={{ fov: 50, near: 0.1, far: 1000, position: [0,30,30] }} shadows={true}>
       <Light />
-      <Floor />
+      <Island />
       <primitive object={new THREE.AxesHelper(100)} />
       <OrbitControls />
     </Canvas>
