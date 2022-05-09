bcf5d53f38be245b085f53a6437778129850c09c
yejineee <yygeniee@gmail.com>
2022-05-09 22:44:03 +0900

    Style: eslint 스타일에 맞춰서 코드 스타일 수정

--- src/components/Rock/Rock.tsx
+++ src/components/Rock/Rock.tsx
@@ -4,12 +4,12 @@ import { FLOOR_SIZE } from '../Floor';
 const X = 0;
 const Z = 2;
 
-const { radiusBottom: FLOOR_RADIUS } = FLOOR_SIZE;
+const { radiusBottom: FLOOR_RADIUS, } = FLOOR_SIZE;
 
 const RADIUS = {
   SMALL: FLOOR_RADIUS / 5,
   MEDIUM: FLOOR_RADIUS / 3,
-  BIG: FLOOR_RADIUS / 2
+  BIG: FLOOR_RADIUS / 2,
 };
 
 const BASE_RANDOM = FLOOR_RADIUS / 10;
@@ -19,28 +19,28 @@ const getRadius = (size) => {
   return Math.min(radius, RADIUS.BIG);
 };
 
-const CENTER_ROCK = { radius: getRadius('BIG'), position: [0, -5, 0] }; // center
+const CENTER_ROCK = { radius: getRadius('BIG'), position: [0, -5, 0], }; // center
 
 const POSITION = {
   EAST: CENTER_ROCK.position[X] + CENTER_ROCK.radius,
   WEST: CENTER_ROCK.position[X] - CENTER_ROCK.radius,
   SOUTH: CENTER_ROCK.position[Z] + CENTER_ROCK.radius,
-  NORTH: CENTER_ROCK.position[Z] - CENTER_ROCK.radius
+  NORTH: CENTER_ROCK.position[Z] - CENTER_ROCK.radius,
 };
 
 const SMALL_ROCK = [
-  { radius: getRadius('SMALL'), position: [POSITION.EAST, 0, POSITION.NORTH] }, // 북동
-  { radius: getRadius('SMALL'), position: [POSITION.WEST, 0, POSITION.NORTH] }, // 북서
-  { radius: getRadius('SMALL'), position: [POSITION.EAST, 0, POSITION.SOUTH] }, // 남동
-  { radius: getRadius('SMALL'), position: [POSITION.WEST, 0, POSITION.SOUTH] } // 남서
+  { radius: getRadius('SMALL'), position: [POSITION.EAST, 0, POSITION.NORTH], }, // 북동
+  { radius: getRadius('SMALL'), position: [POSITION.WEST, 0, POSITION.NORTH], }, // 북서
+  { radius: getRadius('SMALL'), position: [POSITION.EAST, 0, POSITION.SOUTH], }, // 남동
+  { radius: getRadius('SMALL'), position: [POSITION.WEST, 0, POSITION.SOUTH], } // 남서
 
 ];
 
 const MEDIUM_ROCK = [
-  { radius: getRadius('MEDIUM'), position: [POSITION.EAST, 0, 0] }, // 동
-  { radius: getRadius('MEDIUM'), position: [POSITION.WEST, 0, 0] }, // 서
-  { radius: getRadius('MEDIUM'), position: [0, 0, POSITION.SOUTH] }, // 남
-  { radius: getRadius('MEDIUM'), position: [0, 0, POSITION.NORTH] } // 북
+  { radius: getRadius('MEDIUM'), position: [POSITION.EAST, 0, 0], }, // 동
+  { radius: getRadius('MEDIUM'), position: [POSITION.WEST, 0, 0], }, // 서
+  { radius: getRadius('MEDIUM'), position: [0, 0, POSITION.SOUTH], }, // 남
+  { radius: getRadius('MEDIUM'), position: [0, 0, POSITION.NORTH], } // 북
 ];
 
 const DATA = [
