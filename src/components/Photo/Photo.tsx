8c2bb440a6cb8b269249b872e135ab5b82f8172e
yejineee <yygeniee@gmail.com>
2022-05-13 23:51:40 +0900

    Feat: 원으로 둘러 싸기

--- src/components/Photo/Photo.tsx
+++ src/components/Photo/Photo.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react/cjs/react.production.min';
 import { DoubleSide, PlaneGeometry } from 'three';
 
 interface BasePhotoProps {
@@ -15,9 +16,42 @@ const BasePhoto = (props: BasePhotoProps) => {
   );
 };
 
+const degreeToRadian = (degree: number) => degree * Math.PI * (1 / 180);
+
+const calcCirclePosition = ({ degree, radius, }: {degree: number, radius : number}) => {
+  const radian = degreeToRadian(degree);
+  const x = radius * Math.cos(radian);
+  const y = radius * Math.sin(radian);
+
+  return [x, y];
+};
+
+const getCirclePosArray = ({ count, }: {count: number}): ReturnType<typeof calcCirclePosition>[] => {
+  const baseDegree = 360 / count;
+  const radius = 100;
+  const arr = [];
+
+  for (let i = 0; i < count; i++) {
+    arr.push(calcCirclePosition({ degree: baseDegree * i, radius, }));
+  }
+
+  return arr;
+};
+
+const circlePosArray = getCirclePosArray({ count: 12, });
+
 const Photo = () => {
   return (
-    <BasePhoto args={[10, 10]} position={[0, 0, 0]} />
+    <>
+      {circlePosArray.map(pos =>
+        <BasePhoto
+          key={`x:${pos[0]}y:${pos[1]}`}
+          args={[10, 10]}
+          position={[...pos, 0]}
+          />
+      )}
+    </>
+
   );
 };
 
