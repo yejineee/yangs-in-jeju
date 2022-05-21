47716e778b1dc52be376e5d710999653b0b9a480
yejineee <yygeniee@gmail.com>
2022-05-21 12:07:16 +0900

    Update: 클릭시 카메라 무빙을 photo 컴포넌트에서 수행

--- src/components/Photo/BasePhoto.tsx
+++ src/components/Photo/BasePhoto.tsx
@@ -1,30 +1,19 @@
-import { useState, useRef } from 'react';
-import { DoubleSide, PlaneGeometry, Vector3, TextureLoader } from 'three';
-import { useThree, useFrame, useLoader } from '@react-three/fiber';
+import { useRef } from 'react';
+import { DoubleSide, PlaneGeometry, TextureLoader } from 'three';
+import { useLoader } from '@react-three/fiber';
 
 type Position = [number, number, number]
 
 interface BasePhotoProps {
   position: Position;
   args: ConstructorParameters<typeof PlaneGeometry>;
-  color?: string;
+  onClick: any;
 }
-
-const DISTANCE_FROM_PHOTO = 8;
 
 const WIREFRAME = 1;
 
-const BasePhoto = ({ position, args, color, }: BasePhotoProps) => {
-  const state = useThree();
+const BasePhoto = ({ position, args, onClick, }: BasePhotoProps) => {
   const ref = useRef();
-  const [clicked, setClicked] = useState(false);
-
-  useFrame(() => {
-    if (clicked) {
-      state.camera.position.lerp(new Vector3(...position.slice(0, 2), position[2] + DISTANCE_FROM_PHOTO), 0.1);
-      state.camera.lookAt(...position);
-    }
-  });
 
   const colorMap = useLoader(TextureLoader, 'cs.png');
 
@@ -34,7 +23,7 @@ const BasePhoto = ({ position, args, color, }: BasePhotoProps) => {
         <planeBufferGeometry attach='geometry' args={[args[0] + WIREFRAME, args[1] + WIREFRAME]} />
         <meshBasicMaterial color={'#dde4e7'} side={DoubleSide} />
       </mesh>
-      <mesh ref={ref} position={position} onClick={() => setClicked((prev) => !prev)}>
+      <mesh ref={ref} position={position} onClick={() => onClick(position)}>
         <planeBufferGeometry attach='geometry' args={args} />
         <meshBasicMaterial attach="material" map={colorMap} />
       </mesh>
--- src/components/Photo/Photo.tsx
+++ src/components/Photo/Photo.tsx
@@ -1,9 +1,35 @@
+import { useState } from 'react';
 import { getCirclePosArray } from '../../utils/geometry';
 import BasePhoto from './BasePhoto';
+import { useThree, useFrame } from '@react-three/fiber';
+import { Vector3 } from 'three';
 
 const circlePosArray = getCirclePosArray({ count: 12, radius: 60, });
 
+const DISTANCE_FROM_PHOTO = 8;
+
+const MODE = {
+  ISLAND: true,
+  PHOTO: false,
+};
+
 const Photo = () => {
+  const [mode, setMode] = useState(MODE.ISLAND);
+  const [curPhotoPos, setCurPhotoPos] = useState(null);
+  const state = useThree();
+
+  useFrame(() => {
+    if (mode === MODE.PHOTO) {
+      state.camera.position.lerp(new Vector3(...curPhotoPos.slice(0, 2), curPhotoPos[2] + DISTANCE_FROM_PHOTO), 0.1);
+      state.camera.lookAt(...curPhotoPos);
+    }
+  });
+
+  const onClick = (position) => {
+    setMode(!mode);
+    setCurPhotoPos(position);
+  };
+
   return (
     <>
       {circlePosArray.map(pos =>
@@ -11,6 +37,7 @@ const Photo = () => {
           key={`x:${pos[0]}y:${pos[1]}`}
           args={[10, 10]}
           position={[...pos, 0]}
+          onClick={onClick}
           />
       )}
     </>
