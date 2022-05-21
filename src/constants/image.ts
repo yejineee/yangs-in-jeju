c1d326fe4b1b6dfabdb925954e80ca24f1771e71
yejineee <yygeniee@gmail.com>
2022-05-21 23:31:05 +0900

    Add: 이미지 url 추가

---
+++ src/constants/image.ts
@@ -0,0 +1,14 @@
+
+export const IMAGE_NAME = Object.freeze([
+  'kakaofriends1.png',
+  'kakaofriends2.jpeg',
+  'kakaofriends3.png',
+  'kakaofriends4.jpeg',
+  'kakaofriends5.jpeg',
+  'kakaofriends6.jpeg',
+  'kakaofriends7.jpeg',
+  'kakaofriends8.jpeg',
+  'kakaofriends9.png',
+  'kakaofriends10.jpeg',
+  'kakaofriends11.png'
+]);
